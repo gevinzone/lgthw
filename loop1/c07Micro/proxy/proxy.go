b969bad18670eacbb38f4a8aa5d4147fb4d767cc
gevin <flyhigher139@gmail.com>
2022-12-20 22:52:38 +0800

    optimize

--- loop1/c07Micro/proxy/proxy.go
+++ loop1/c07Micro/proxy/proxy.go
@@ -2,6 +2,7 @@ package proxy
 
 import (
 	"bytes"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -24,7 +25,7 @@ func NewDefaultProxy(baseUrl string) *Proxy {
 }
 
 func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if err := p.ProcessRequest(r); err != nil {
+	if err := p.changeRequest(r); err != nil {
 		log.Printf("error occurred during process: %v", err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -35,11 +36,10 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	defer resp.Body.Close()
-	p.CopyResponse(w, resp)
+	p.changAndWriteResponse(w, resp)
 }
 
-func (p *Proxy) ProcessRequest(r *http.Request) error {
+func (p *Proxy) changeRequest(r *http.Request) error {
 	proxyUrlRaw := p.BaseUrl + r.URL.String()
 	proxyUrl, err := url.Parse(proxyUrlRaw)
 	if err != nil {
@@ -51,9 +51,10 @@ func (p *Proxy) ProcessRequest(r *http.Request) error {
 	return nil
 }
 
-func (p *Proxy) CopyResponse(w http.ResponseWriter, resp *http.Response) {
+func (p *Proxy) copyResponse(w http.ResponseWriter, resp *http.Response) {
 	var out bytes.Buffer
 	_, err := out.ReadFrom(resp.Body)
+	_ = resp.Body.Close()
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -66,4 +67,21 @@ func (p *Proxy) CopyResponse(w http.ResponseWriter, resp *http.Response) {
 	}
 	w.WriteHeader(resp.StatusCode)
 	_, _ = w.Write(out.Bytes())
+}
+
+func (p *Proxy) changAndWriteResponse(w http.ResponseWriter, r *http.Response) {
+	data, err := ioutil.ReadAll(r.Body)
+	_ = r.Body.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	for key, values := range r.Header {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+	w.WriteHeader(r.StatusCode)
+	_, _ = w.Write(data)
 }
