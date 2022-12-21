449dbd788a8a04578f69c442982e13cee6ded042
gevin <flyhigher139@gmail.com>
2022-12-21 17:22:33 +0800

    grpc example

---
+++ loop1/c07Micro/grpc/client/client.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/gevinzone/lgthw/loop1/c07Micro/grpc/proto/gen"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	target := ":8888"
+	cc, err := grpc.Dial(target, grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	client := gen.NewGreeterClient(cc)
+	in := &gen.HelloRequest{Name: "Gevin"}
+	reply, err := client.SayHello(context.Background(), in)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(reply)
+}
---
+++ loop1/c07Micro/grpc/server/server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/gevinzone/lgthw/loop1/c07Micro/grpc/proto/gen"
+	"google.golang.org/grpc"
+	"net"
+)
+
+type HelloServer struct {
+	gen.UnimplementedGreeterServer
+}
+
+func (h *HelloServer) SayHello(ctx context.Context, request *gen.HelloRequest) (*gen.HelloReply, error) {
+	reply := &gen.HelloReply{Message: "Hello, " + request.GetName()}
+	return reply, nil
+}
+
+func main() {
+	server := grpc.NewServer()
+	gen.RegisterGreeterServer(server, &HelloServer{})
+	address := ":8888"
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Start serve on %s\n", address)
+	server.Serve(listener)
+}
