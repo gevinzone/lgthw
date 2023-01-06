dea318c5f5dacab5d8ad18320eaa70b205ff1c06
gevin <flyhigher139@gmail.com>
2023-01-06 10:30:06 +0800

    refactor

--- loop1/c06WebClient/oauth/basic/api.go
+++ loop1/c06WebClient/oauth/basic/api.go
@@ -1,4 +1,4 @@
-package oauth
+package basic
 
 import (
 	"fmt"
---
+++ loop1/c06WebClient/oauth/basic/example/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	"github.com/gevinzone/lgthw/loop1/c06WebClient/oauth/basic"
+)
+
+func main() {
+	conf := basic.Setup()
+	token, err := basic.GetToken(context.Background(), conf)
+	if err != nil {
+		panic(err)
+	}
+	client := conf.Client(context.Background(), token)
+	if err = basic.GetUser(client); err != nil {
+		panic(err)
+	}
+}
