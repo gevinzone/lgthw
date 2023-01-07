c7150101dc609b36c5f160bd1fcdcc22c322de38
gevin <flyhigher139@gmail.com>
2023-01-07 17:24:55 +0800

    mysql

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/gevinzone/lgthw
 go 1.18
 
 require (
+	github.com/go-sql-driver/mysql v1.7.0
 	github.com/stretchr/testify v1.8.1
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	google.golang.org/grpc v1.51.0
---
+++ loop1/c07DB/db/basic/example/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gevinzone/lgthw/loop1/c07DB/db/basic"
+)
+
+func main() {
+	dataSource := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/go?charset=utf8mb4&parseTime=true&loc=Local", "gevin", "gevin")
+	db, err := basic.Open(dataSource)
+	if err != nil {
+		panic(err)
+	}
+	err = basic.Exec(db)
+	if err != nil {
+		panic(err)
+	}
+}
