f067509ba9d733b82f4f7fca5f2500c9640d61e5
gevin <flyhigher139@gmail.com>
2023-01-25 22:20:45 +0800

    add more example

--- loop1/c08Test/benchmark/command.sh
+++ loop1/c08Test/benchmark/command.sh
@@ -1,1 +1,2 @@
 go test -bench=^BenchmarkExample$ -benchtime=1000x -benchmem -cpu=2,3,4 .
+go test -bench=Concat -benchtime=1s -benchmem .
--- loop1/c08Test/benchmark/example_test.go
+++ loop1/c08Test/benchmark/example_test.go
@@ -1,6 +1,7 @@
 package benchmark
 
 import (
+	"bytes"
 	"fmt"
 	"testing"
 	"time"
@@ -78,4 +79,28 @@ func BenchmarkExample4(b *testing.B) {
 		time.Sleep(time.Microsecond)
 		_ = fmt.Sprintf("something, %s", "something")
 	}
+}
+
+func BenchmarkConcatStringByAdd(b *testing.B) {
+	elems := []string{"1", "2", "3", "4", "5"}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ret := ""
+		for _, elem := range elems {
+			ret += elem
+		}
+	}
+}
+
+func BenchmarkConcatStringByBytesBuffer(b *testing.B) {
+	elems := []string{"1", "2", "3", "4", "5"}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		var buf bytes.Buffer
+		for _, elem := range elems {
+			buf.WriteString(elem)
+		}
+		_ = buf.String()
+	}
+
 }
