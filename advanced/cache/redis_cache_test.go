f3d61d6f4dc6b26e7ac424a2790e8e2c3ceebf45
gevin <flyhigher139@gmail.com>
2023-02-10 11:00:43 +0800

    redis mock test

---
+++ advanced/cache/redis_cache_test.go
@@ -0,0 +1,88 @@
+package cache
+
+import (
+	"context"
+	"fmt"
+	"github.com/gevinzone/lgthw/advanced/cache/mocks"
+	"github.com/go-redis/redis/v9"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+//go:generate mockgen -destination=./mocks/redis_mock.go -package=mocks github.com/go-redis/redis/v9 Cmdable
+
+func TestRedisCache_Set(t *testing.T) {
+	testCases := []struct {
+		name       string
+		key        string
+		val        string
+		expiration time.Duration
+		mock       func(ctrl *gomock.Controller) redis.Cmdable
+		wantErr    error
+	}{
+		{
+			name:       "normal",
+			key:        "key1",
+			val:        "value1",
+			expiration: time.Second,
+			mock: func(ctrl *gomock.Controller) redis.Cmdable {
+				cmd := mocks.NewMockCmdable(ctrl)
+				//status := redis.NewStatusCmd(context.Background())
+				//status.SetVal("OK")
+				cmd.EXPECT().
+					Set(gomock.Any(), "key1", "value1", time.Second).
+					Return(redis.NewStatusResult("OK", nil))
+				return cmd
+			},
+		},
+		{
+			name:       "timeout",
+			key:        "key1",
+			val:        "value1",
+			expiration: time.Second,
+			mock: func(ctrl *gomock.Controller) redis.Cmdable {
+				cmd := mocks.NewMockCmdable(ctrl)
+				cmd.EXPECT().
+					Set(gomock.Any(), "key1", "value1", time.Second).
+					Return(redis.NewStatusResult("", context.DeadlineExceeded))
+				return cmd
+			},
+			wantErr: context.DeadlineExceeded,
+		},
+		{
+			name:       "unexpected msg",
+			key:        "key1",
+			val:        "value1",
+			expiration: time.Second,
+			mock: func(ctrl *gomock.Controller) redis.Cmdable {
+				cmd := mocks.NewMockCmdable(ctrl)
+				cmd.EXPECT().
+					Set(gomock.Any(), "key1", "value1", time.Second).
+					Return(redis.NewStatusResult("something", nil))
+				return cmd
+			},
+			wantErr: fmt.Errorf("%w, 返回信息 %s", errFailedToSetCache, "something"),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			client := NewRedisCache(tc.mock(ctrl))
+			err := client.Set(context.Background(), tc.key, tc.val, tc.expiration)
+			assert.Equal(t, tc.wantErr, err)
+		})
+	}
+}
+
+func TestRedisCache_Get(t *testing.T) {
+}
+
+func TestRedisCache_Delete(t *testing.T) {
+}
+
+func TestRedisCache_LoadAndDelete(t *testing.T) {
+
+}
--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.18
 require (
 	github.com/go-redis/redis/v9 v9.0.0-rc.2
 	github.com/go-sql-driver/mysql v1.7.0
+	github.com/golang/mock v1.6.0
 	github.com/stretchr/testify v1.8.1
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	golang.org/x/text v0.4.0
 	google.golang.org/grpc v1.51.0
 	google.golang.org/protobuf v1.28.1
 	gopkg.in/yaml.v3 v3.0.1
@@ -18,9 +20,10 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
-	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/tools v0.1.12 // indirect
 	google.golang.org/appengine v1.4.0 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 )
