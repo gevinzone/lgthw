5d637b9f55407bfd273b19acc6aa1b150ae2c97d
gevin <flyhigher139@gmail.com>
2023-02-13 15:05:33 +0800

    init cache and goroutine for deleting expired cache

--- advanced/cache/local_cache.go
+++ advanced/cache/local_cache.go
@@ -21,13 +21,55 @@ func WrapErrKeyExpired(key string) error {
 	return fmt.Errorf("%w, key is %s", errKeyExpired, key)
 }
 
+type BuildInCacheOption func(cache *BuildInCache)
+
 // BuildInCache 基于内存的本地缓存
 // 1. 并发安全
 // 2. 过期删除机制：（1）延时删除，即Get()数据时，发现数据过期，则删除 （2）轮询定时删除一定数量的数据
 type BuildInCache struct {
-	mutex     sync.RWMutex
-	data      map[string]*item
-	onEvicted func(key string, val any)
+	mutex       sync.RWMutex
+	data        map[string]*item
+	onEvicted   func(key string, val any)
+	closeSignal chan struct{}
+}
+
+func NewBuildInCache(interval time.Duration, opts ...BuildInCacheOption) *BuildInCache {
+	cache := &BuildInCache{
+		data: make(map[string]*item, 100),
+		onEvicted: func(key string, val any) {
+
+		},
+		closeSignal: make(chan struct{}, 1),
+	}
+
+	for _, opt := range opts {
+		opt(cache)
+	}
+
+	go func() {
+		ticker := time.NewTicker(interval)
+		for {
+			select {
+			case <-cache.closeSignal:
+				return
+			case t := <-ticker.C:
+				count, max := 0, 1000
+				cache.mutex.Lock()
+				for key, itm := range cache.data {
+					if count >= max {
+						break
+					}
+					if !itm.isBeforeDeadline(t) {
+						cache.delete(key)
+					}
+					count++
+				}
+				cache.mutex.Unlock()
+			}
+		}
+	}()
+
+	return cache
 }
 
 func (b *BuildInCache) Set(ctx context.Context, key string, val any, expiration time.Duration) error {
@@ -118,7 +160,7 @@ func (i *item) isExpired() bool {
 }
 
 func (i *item) isBeforeDeadline(t time.Time) bool {
-	return !i.deadline.IsZero() && t.Before(i.deadline)
+	return i.deadline.IsZero() || t.Before(i.deadline)
 }
 
 func (i *item) isAfterDeadline(t time.Time) bool {
