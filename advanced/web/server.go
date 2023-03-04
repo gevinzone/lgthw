0ac07ebc886e291a0cf2e8d95aacc03914e57008
gevin <flyhigher139@gmail.com>
2023-03-04 13:16:39 +0800

    server and context

---
+++ advanced/web/server.go
@@ -0,0 +1,66 @@
+package web
+
+import (
+	"net"
+	"net/http"
+)
+
+type HandleFunc func(ctx *Context)
+
+type Server interface {
+	http.Handler
+	Start(addr string) error
+	AddRoute(method, path string, handler HandleFunc)
+}
+
+type HttpServer struct {
+}
+
+var _ Server = &HttpServer{}
+
+func (s *HttpServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := &Context{
+		Req:  r,
+		Resp: w,
+	}
+	s.serve(ctx)
+}
+
+func (s *HttpServer) serve(ctx *Context) {
+	panic("implement me")
+}
+
+func (s *HttpServer) AddRoute(method, path string, handler HandleFunc) {
+
+}
+
+func (s *HttpServer) Get(path string, handler HandleFunc) {
+	s.AddRoute(http.MethodGet, path, handler)
+}
+
+func (s *HttpServer) Post(path string, handler HandleFunc) {
+	s.AddRoute(http.MethodPost, path, handler)
+}
+
+func (s *HttpServer) Put(path string, handler HandleFunc) {
+	s.AddRoute(http.MethodPut, path, handler)
+}
+
+func (s *HttpServer) Patch(path string, handler HandleFunc) {
+	s.AddRoute(http.MethodPatch, path, handler)
+}
+
+func (s *HttpServer) Delete(path string, handler HandleFunc) {
+	s.AddRoute(http.MethodDelete, path, handler)
+}
+
+func (s *HttpServer) Start(addr string) error {
+	//err := http.ListenAndServe(addr, s)
+	//return err
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+	err = http.Serve(listener, s)
+	return err
+}
