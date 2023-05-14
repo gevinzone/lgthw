6f281f500a66e3739266c35a132d1baf8a4d747b
gevin <flyhigher139@gmail.com>
2023-05-14 16:39:11 +0800

    grpc example

---
+++ advanced/grpc/example/grpc/client_test.go
@@ -0,0 +1,21 @@
+package grpc
+
+import (
+	"context"
+	"github.com/gevinzone/lgthw/advanced/grpc/example/proto/gen"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"testing"
+	"time"
+)
+
+func TestClient(t *testing.T) {
+	cc, err := grpc.Dial(":8000", grpc.WithInsecure())
+	require.NoError(t, err)
+	cli := gen.NewUserServiceClient(cc)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	resp, err := cli.GetById(ctx, &gen.GetByIdReq{Id: 1})
+	require.NoError(t, err)
+	t.Log(resp)
+}
